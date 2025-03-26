0264a45ccef15221e858ad5a486da7a1dde68754
lemofack <lemofack@3il.fr>
2025-03-27 00:34:49 +0100

    tuto 03

--- src/main/java/tuto/web/WebConnexion.java
+++ src/main/java/tuto/web/WebConnexion.java
@@ -1,6 +1,7 @@
 package tuto.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,4 +12,18 @@ public class WebConnexion {
 	 public String bonjour() {
 	 return "bonjour";
 	 }
+ @GetMapping( "/connect" )
+ public String connect(String username,String password,Model model) {
+	 var sb = new StringBuilder(username);
+	 if(sb.reverse().toString().equals( password )) {
+		 model.addAttribute( "connected", username );
+		 return "bonjour"; 
+	 }else {
+		 model.addAttribute( "alert","Mot de passe incorrect" );
+		 return "index";
+	 }
+	 
+	 
+	 
+ }
 }
