ae311848e90d386ba0348e1f6b74a4f10f00bccb
lemofack <lemofack@3il.fr>
2025-04-04 08:35:35 +0200

    tuto4

--- src/main/java/tuto/web/WebConnexion.java
+++ src/main/java/tuto/web/WebConnexion.java
@@ -3,9 +3,11 @@ package tuto.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
-
+@SessionAttributes("connected")
 public class WebConnexion {
 	
  @GetMapping( "/bonjour" )
@@ -13,17 +15,31 @@ public class WebConnexion {
 	 return "bonjour";
 	 }
  @GetMapping( "/connect" )
+ 
  public String connect(String username,String password,Model model) {
+	 
 	 var sb = new StringBuilder(username);
+	 
 	 if(sb.reverse().toString().equals( password )) {
+		 
 		 model.addAttribute( "connected", username );
+		 
 		 return "bonjour"; 
-	 }else {
+	  }else {
+		 
 		 model.addAttribute( "alert","Mot de passe incorrect" );
+		
 		 return "index";
+	  }
 	 }
+ 
+ @GetMapping( "/disconnect" )
+ public String disconnect(SessionStatus status, Model model ) {
 	 
+	 status.setComplete();
+	 model.addAttribute( "connected", "Déconnexion effectuée avec succès");
+	 return "index";
+}
 	 
-	 
- }
+ 
 }
